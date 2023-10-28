7673277168b5781f74f098ea766c3bfcae6a9fa1
Diego Vasque Maldonado <diegoomal13@gmail.com>
2023-10-28 13:34:08 -0300

    update

--- app/main.py
+++ app/main.py
@@ -1,10 +1,30 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# import os
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
+import time
 import tensorflow as tf
 
 
 if __name__ == "__main__":
 
-  print('1) Verifica o acesso a placa de video')
+  print('1) Verify GPU access')
   print(tf.config.list_physical_devices('GPU'))
+
+  def test(proc_env):
+    
+    start_time_cpu = time.time()
+    
+    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+    c = tf.matmul(a, b)
+    
+    print(c)
+    
+    delta_time = time.time() - start_time_cpu
+    print(f'execution time {proc_env}: {delta_time} seconds')
+
+  with tf.device('/cpu:0'):
+    test('cpu')
+
+  with tf.device('/gpu:0'):
+    test('gpu')
--- dockerfile
+++ dockerfile
@@ -1,5 +1,7 @@
 FROM tensorflow/tensorflow:latest-gpu
 
+ENV TF_CPP_MIN_LOG_LEVEL 2
+
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 WORKDIR /app
